8d40149179ccffaa0035438909e45d14df7762cc
Zach Bagley <szachbagley@gmail.com>
2024-02-26 08:39:59 -0700

    Deleted unneseccary stuff

--- Mission06_Bagley/Controllers/HomeController.cs
+++ Mission06_Bagley/Controllers/HomeController.cs
@@ -85,12 +85,6 @@ namespace Mission06_Bagley.Controllers
             return RedirectToAction("Collection");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
         [HttpGet]
         public IActionResult Delete(int id)
         {
